4da8aae1e5b06f4bfa2606e2ba63d4d2d79f0d59
Scheltema <thomas.scheltema@hp.com>
2025-03-10 11:52:28 -0500

    create musicians and band endpoints and tests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
-
+const { Musician } = require("../models/index");
+const { Band } = require("../models/Band");
+const { db } = require("../db/connection");
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+app.get("/musicians", async (req, res) => {
+  const musicians = await Musician.findAll();
+  res.json(musicians);
+});
+
+app.get("/musicians/1", async (req, res) => {
+  const musician = await Musician.findByPk(1);
+  res.json(musician);
+});
+
+app.get("/bands", async (req, res) => {
+  const bands = await Band.findAll();
+  res.json(bands);
+});
 
 
 
